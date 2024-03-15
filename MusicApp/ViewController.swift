af4f182c7717bcba2baa1d55b8fd37a499784550
Han <g43424140@gmail.com>
2024-03-16 01:17:42 +0800

    Initial Commit

---
+++ MusicApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicApp
+//
+//  Created by YaHan on 2024/3/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
